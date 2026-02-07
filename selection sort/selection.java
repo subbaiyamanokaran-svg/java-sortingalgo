91cebce88f054da862197b6a67fcf08eb015eff2
subbaiyamanokaran-svg <subbaiya.manokaran@gmail.com>
2026-02-07 23:28:30 +0530

    Started with Selection Sort

---
+++ selection sort/selection.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Selection{
+    public static void main(String asd[]){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]= new int[n];
+
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+        
+        Selectionsort(arr);
+
+        for(int num :arr)
+        System.out.print(num+" ");
+    }
+
+    public static void Selectionsort(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            
+            int min=i;
+
+            for (int j=i+1;j<n;j++){
+                if (arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp= arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+    }
+}
